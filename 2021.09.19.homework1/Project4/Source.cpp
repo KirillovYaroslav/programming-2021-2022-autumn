5333741071c8e0e2103804961162d0ba57aca6f4
KirillovYaroslav <90608393+KirillovYaroslav@users.noreply.github.com>
2021-09-21 21:59:21 +0300

    Update Source.cpp

--- 2021.09.19.homework1/Project4/Source.cpp
+++ 2021.09.19.homework1/Project4/Source.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	int v, t;
+	int v=0, t=0;
 	cin >> v >> t;
 	int s = v * t;
-
 	cout << ((s % 109) + 109) % 109;
+	return EXIT_SUCCESS;
 }
