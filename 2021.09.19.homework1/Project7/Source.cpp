2c501fa66b571780395b979a3cbee4ec941dbb8a
KirillovYaroslav <90608393+KirillovYaroslav@users.noreply.github.com>
2021-09-21 22:00:26 +0300

    Update Source.cpp

--- 2021.09.19.homework1/Project7/Source.cpp
+++ 2021.09.19.homework1/Project7/Source.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	int a, b, c;
+	int a=0, b=0, c=0;
 	cin >> a >> b;
 	c = a;
 	a = b;
 	b = c;
 	cout << a<<" "<< b;
+	return EXIT_SUCCESS;
 }
