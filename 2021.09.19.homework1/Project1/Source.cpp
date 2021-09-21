1ea16ba16a63a77ba1bd23397caa45ae62e65fa4
KirillovYaroslav <90608393+KirillovYaroslav@users.noreply.github.com>
2021-09-21 21:54:05 +0300

    Update Source.cpp

--- 2021.09.19.homework1/Project1/Source.cpp
+++ 2021.09.19.homework1/Project1/Source.cpp
@@ -4,6 +4,6 @@ int main(int argc, char* argv[])
 {
 	int a;
 	cin >> a;
-	cout << "The next number for the number " << a << " is " << a + 1 << endl;
-	cout << "The previous number for the number " << a << " is " << a - 1 << endl;
+	cout << "The next number for the number " << a << " is " << a + 1 <<"."<< endl;
+	cout << "The previous number for the number " << a << " is " << a - 1<<"."<< endl;
 }
