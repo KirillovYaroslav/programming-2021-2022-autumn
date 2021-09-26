560d9a895510d1047d231b64013f62b8dfa8f796
KirillovYaroslav <90608393+KirillovYaroslav@users.noreply.github.com>
2021-09-26 16:39:29 +0300

    Update Source.cpp

--- 2021.09.23-lesson2-homework/Project4/Source.cpp
+++ 2021.09.23-lesson2-homework/Project4/Source.cpp
@@ -1,1 +1,49 @@
-#include<iostream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	double x;
+	cin >> a >> b >> c >> d;
+	if (c == 0 && d == 0)
+	{
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		if (a != 0)
+		{
+			if (b % a == 0)
+			{
+				x = -b / a;
+				if (c * x + d == 0)
+				{
+					cout << "NO";
+				}
+				else
+				{
+					cout << x;
+				}
+			}
+			else
+			{
+				cout << "NO";
+			}
+		}
+		else
+		{
+			if (b == 0)
+			{
+				cout << "INF";
+			}
+		}
+
+	}
+	return EXIT_SUCCESS;
+}
