0fbfaf83e1c3c472d2ce07404b84d1ffa5cf4242
KirillovYaroslav <st082091@student.spbu.ru>
2021-10-02 16:47:08 +0300

    Homework

--- 2021.09.23-lesson2-homework/Project8/Source.cpp
+++ 2021.09.23-lesson2-homework/Project8/Source.cpp
@@ -2,43 +2,74 @@
 
 using namespace std;
 
-int sred(int a, int b, int c)
-{
-	if ((b >= a && a >= c) || (c >= a && a >= b))
-	{
-		return a;
-	}
-	else
-	{
-		if ((a >= b && b >= c) || (c >= b && b >= a))
-		{
-			return b;
-		}
-		else
-		{
-			return c;
-		}
-	}
-
-}
 
 int main(int argc, char* argv[])
 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
+	int sred = 0;
+	int max = 0;
+	int min = 0;
 	cin >> a >> b >> c;
 
+	if ((b >= a && a >= c) || (c >= a && a >= b))
+	{
+		sred = a;
+	}
+	else
+	{
+		if ((a >= b && b >= c) || (c >= b && b >= a))
+		{
+			sred = b;
+		}
+		else
+		{
+			sred = c;
+		}
+	}
+
+	if (a >= b && a >= c)
+	{
+		max = a;
+	}
+	else
+	{
+		if (a <= b && b >= c)
+		{
+			max = b;
+		}
+		else
+		{
+			max = c;
+		}
+	}
+
+	if (a <= b && a <= c)
+	{
+		min = a;
+	}
+	else
+	{
+		if (a >= b && b <= c)
+		{
+			min = b;
+		}
+		else
+		{
+			min = c;
+		}
+	}
 
 	if (a < b + c && b < a + c && c < a + b)
 	{
-		if ((-max(a, max(b, c)) * max(a, max(b, c)) + sred(a, b, c) * sred(a, b, c) + min(a, min(b, c)) * min(a, min(b, c))) < 0)
+		if ((-max * max + sred * sred + min * min) < 0)
 		{
 			cout << "obtuse";
 		}
 		else
 		{
-			if ((-max(a, max(b, c)) * max(a, max(b, c)) + sred(a, b, c) * sred(a, b, c) + min(a, min(b, c)) * min(a, min(b, c))) > 0)
+			if (-max * max + sred * sred + min * min > 0)
 			{
 				cout << "acute";
 			}
