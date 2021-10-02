e2edb2c975741a654381f7838806a67664763713
KirillovYaroslav <st082091@student.spbu.ru>
2021-10-02 17:10:48 +0300

    Homework

--- 2021.09.23-lesson2-homework/Project6/Source.cpp
+++ 2021.09.23-lesson2-homework/Project6/Source.cpp
@@ -8,10 +8,35 @@ int main(int argc, char* argv[])
 	int y = 0;
 	int x1 = 0;
 	int y1 = 0;
+	int max1 = 0;
+	int min1 = 0;
+	int max2 = 0;
+	int min2 = 0;
 	
+	if (x > x1)
+	{
+		max1 = x;
+		min1 = x1;
+	}
+	else
+	{
+		max1 = x1;
+		min1 = x;
+	}
+	if (y > y1)
+	{
+		max2 = y;
+		min2 = y1;
+	}
+	else
+	{
+		max2 = y1;
+		min2 = y;
+	}
+
 	cin >> x >> y >> x1 >> y1;
 
-	if ((x == x1) || (y == y1) || (max(x, x1) - min(x, x1) == max(y, y1) - min(y, y1)))
+	if ((x == x1) || (y == y1) || (max1 - min1 == max2 - min2))
 	{
 		cout << "YES";
 	}
