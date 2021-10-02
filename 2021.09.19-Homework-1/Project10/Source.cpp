e2e1f702dec5293ba938ce3d8a924c2059522483
KirillovYaroslav <st082091@student.spbu.ru>
2021-10-02 19:13:28 +0300

    Homework

--- 2021.09.19-Homework-1/Project10/Source.cpp
+++ 2021.09.19-Homework-1/Project10/Source.cpp
@@ -7,10 +7,10 @@ int main()
 {
     int a = 0;
     int b = 0;
-
+    
     cin >> a >> b;
 
-    cout << (a * (a / b) + b * (b / a)) / (b / a + a / b);
+    cout << (a * (a / b) + b * (b / a)) / (a / b + b / a);
 
     return EXIT_SUCCESS;
 }
