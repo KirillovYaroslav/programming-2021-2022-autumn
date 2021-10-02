7252cca74d1630ca5a21c48bbbd74ca46e0aaede
KirillovYaroslav <st082091@student.spbu.ru>
2021-10-02 16:59:32 +0300

    Homework

--- 2021.09.23-lesson2-homework/Project7/Source.cpp
+++ 2021.09.23-lesson2-homework/Project7/Source.cpp
@@ -10,28 +10,21 @@ int main()
 
     cin >> n >> m >> k;
 
-    if (k != m * n)
+    if (k % n == 0 && k / n <= m && k != m * n)
     {
-        if (k % n == 0 && k / n <= m)
+        cout << "YES";
+    }
+    else
+    {
+        if (k % m == 0 && k / m <= n && k != m * n)
         {
             cout << "YES";
         }
         else
         {
-            if (k % m == 0 && k / m <= n)
-            {
-                cout << "YES";
-            }
-            else
-            {
-                cout << "NO";
-            }
+            cout << "NO";
         }
+    }
 
-        return EXIT_SUCCESS;
-    }
-    else
-    {
-        return EXIT_SUCCESS;
-    }
+    return EXIT_SUCCESS;
 }
