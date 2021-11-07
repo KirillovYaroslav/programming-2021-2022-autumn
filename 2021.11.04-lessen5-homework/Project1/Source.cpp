74b902988286b878d9d0beed3ba586759611c75a
KirillovYaroslav <st082091@student.spbu.ru>
2021-11-07 22:59:59 +0300

    Homework

--- 2021.11.04-lessen5-homework/Project1/Source.cpp
+++ 2021.11.04-lessen5-homework/Project1/Source.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	char c;
-	cin >> c;
-	cout << int(c);
+	float n = 0;
+
+	cin >> n;
+
+	int value = *((int*) & n);
+	int bitmask = 1;
+	bitmask = ~ (bitmask << (sizeof(int) * 8 - 1));
+	value = value & bitmask;
+	float abc_n = *((float*)&value);
+
+	cout << abc_n;
+
+	return EXIT_SUCCESS;
 }
